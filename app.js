d9dfe25e4d58b8b2fa74a1e72b08e862a2a338a8
mtslck <mats.loock@lnu.se>
2017-05-18 14:36:07 +0200

    Squashed 'part-1/simple-addition/' content from commit 875abff
    git-subtree-dir: part-1/simple-addition
    git-subtree-split: 875abff527605f145d929ff7e1aba1f5a8dc6a6c

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * The starting  point of the application.
+ *
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+// Get the module "calc". Using ./src/ to indicate that it is
+// placed in a subdirectory of this directory.
+const calc = require('./src/calc')
+
+// Call the function "addNumber" in the calc module without
+// any arguments and save the result in a new local variable.
+// TODO: Write your code here.
+
+// Print the result to the terminal (console).
+// TODO: Write your code here.

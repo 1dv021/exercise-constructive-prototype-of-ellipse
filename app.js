5d5196315baad9bfbf5bee9405a1e111e0c56d15
mtslck <mats.loock@lnu.se>
2017-05-18 09:22:44 +0200

    Squashed 'tiny-funcs/' content from commit b935bab
    git-subtree-dir: tiny-funcs
    git-subtree-split: b935bab0d7743b71dfc6c8679ce749272eeb6f84

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * The starting  point of the application.
+ *
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+console.log('Run the tests by \'npm test\'.')

bb489e4640ec1dc5b1caa1746c193f4e7759b5ef
Mats Loock <mats.loock@lnu.se>
2017-05-31 11:31:35 +0200

    Update shape.js

--- part-1/right-triangle/src/shape.js
+++ part-1/right-triangle/src/shape.js
@@ -11,8 +11,8 @@
 /**
  * Returns a string representing a right triangle.
  *
- * @param {Number} base The number of characters in the triangle's base.
- * @returns {String}
+ * @param {number} base The number of characters in the triangle's base.
+ * @returns {string} A string of hash characters.
  */
 function createRightTriangle (base) {
   let triangle = ''

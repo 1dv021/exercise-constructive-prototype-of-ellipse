b0a327b9ce58c01a56a1cd0bbdd0c398b3aa2103
mtslck <mats.loock@lnu.se>
2017-08-15 13:11:24 +0200

    Squashed 'part-1/abacus/' content from commit 8e8066b
    git-subtree-dir: part-1/abacus
    git-subtree-split: 8e8066bbff7fea51d5bfc3cd905bc5deb03c1b87

---
+++ src/abacus.js
@@ -0,0 +1,23 @@
+/**
+ * Module for a very simple adding machine.
+ *
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+/**
+ * Returns the sum of the arguments passed.
+ *
+ * @param {...*} varArgs
+ * @returns {Number} The sum of an arbitrary number of numbers.
+ */
+function add (varArgs) {
+  // Utilize the 'arguments' object here, not the 'varArgs' parameter.
+
+  // TODO: Write your code here.
+}
+
+exports.add = add
---
+++ test/abacus.test.js
@@ -0,0 +1,78 @@
+/**
+ * Tests for the abacus module.
+ *
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+const abacus = require('../src/abacus')
+const expect = require('chai').expect
+
+describe('Test the module with kind values', () => {
+  it('Should return 9 when called with 3, 6', done => {
+    expect(abacus.add(3, 6)).eql(9)
+    done()
+  })
+
+  it('Should return 50 when called with five arguments a 10', done => {
+    expect(abacus.add(10, 10, 10, 10, 10)).eql(50)
+    done()
+  })
+
+  it('Should return 5 when called with just one argument (5)', done => {
+    expect(abacus.add(5)).eql(5)
+    done()
+  })
+
+  it('Should return 0 when called without argument', done => {
+    expect(abacus.add()).eql(0)
+    done()
+  })
+
+  it('Should handle 0 as a parameter', done => {
+    expect(abacus.add(2, 3, 0)).eql(5)
+    done()
+  })
+
+  it('Should return 0 when called -1 and 1', done => {
+    expect(abacus.add(1, -1)).eql(0)
+    done()
+  })
+
+  it('Should handle negative results', done => {
+    expect(abacus.add(-1, -1, -1, -1)).eql(-4)
+    expect(abacus.add(1, -1, -1)).eql(-1)
+    done()
+  })
+})
+
+describe('Test the module with non Number values', () => {
+  it('Should return 12 when called with with three strings ' +
+    'that are text as numbers e.g. (\'4\', \'4\', \'4\')', done => {
+    expect(abacus.add('4', '4', '4')).eql(12)
+    done()
+  })
+
+  it('Should return 6.5 when called with decimal number (as dot) as strings', done => {
+    expect(abacus.add('3.5', 3)).eql(6.5)
+    done()
+  })
+
+  it('Should return correct when called with decimal number (with a comma) as strings', done => {
+    expect(abacus.add(2.5, '3,5')).eql(6)
+    done()
+  })
+
+  it('Should return NaN (Not-A-Number) when called with a string that cannot be converted to a Number.', done => {
+    expect(abacus.add(1, 'lorem ipsum', 1)).eql(NaN)
+    done()
+  })
+
+  it('Should return NaN (Not-A-Number) when called with a boolean', done => {
+    expect(abacus.add(3, 3, true)).eql(NaN)
+    done()
+  })
+})

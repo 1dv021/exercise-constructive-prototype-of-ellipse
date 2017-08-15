b38fb0b0c8a6f6816a931d23d1ec09c8399622b2
mtslck <mats.loock@lnu.se>
2017-08-15 10:33:44 +0200

    Add .gitignore

--- part-1/reduced-array/src/my-array.js
+++ part-1/reduced-array/src/my-array.js
@@ -12,11 +12,16 @@
 /**
  * Takes an array of numbers and returns the sum of all numbers in the array.
  *
- * @param {number[]} data An array with numbers
+ * @param {number[]} source An array with numbers
  * @returns {number} The total sum of the numbers in the array.
 */
-function getSum (data) {
-  // TODO: Write your code here.
+function getSum (source) {
+  // Is it an array?
+  if (!Array.isArray(source)) {
+    throw new TypeError('The argument passed must be an array.')
+  }
+
+  return source.reduce((prev, curr) => prev + curr, 0)
 }
 
 exports.getSum = getSum
